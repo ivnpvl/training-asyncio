94f0234d71db6622d6608cc91ff25ca93cc27486
Ivan Pavlov <ivnpvl@disroot.org>
2023-12-02 14:25:19 +0300

    Refactor echo server with asyncio

--- echo_server.py
+++ echo_server.py
@@ -1,25 +1,29 @@
+import asyncio as ao
 import socket as st
+from asyncio import AbstractEventLoop as AELoop
 
 
-server_socket = st.socket(st.AF_INET, st.SOCK_STREAM)
-server_socket.setsockopt(st.SOL_SOCKET, st.SO_REUSEADDR, 1)
+async def echo(connection: st.socket, loop: AELoop) -> None:
+    while data := await loop.sock_recv(connection, 1024):
+        await loop.sock_sendall(connection, data)
 
-server_address = '127.0.0.1', 8000
-server_socket.bind(server_address)
-server_socket.listen()
 
-try:
-    connection, client_address = server_socket.accept()
-    print(f'Получен запрос на подключение от {client_address}.')
-    buffer = b''
-    while buffer[-2::] != b'\r\n':
-        data = connection.recv(2)
-        if not data:
-            break
-        else:
-            print(f'Получены данные: {data}.')
-            buffer += data
-    print(f'Полные данные: {buffer}.')
-    connection.sendall(buffer)
-finally:
-    server_socket.close()
+async def listen_for_connection(server_socket: st.socket, loop: AELoop):
+    while True:
+        connection, client_address = await loop.sock_accept(server_socket)
+        connection.setblocking(False)
+        print(f'Получен запрос на подключение от {client_address}.')
+        ao.create_task(echo(connection, loop))
+
+
+async def main():
+    server_socket = st.socket(st.AF_INET, st.SOCK_STREAM)
+    server_socket.setsockopt(st.SOL_SOCKET, st.SO_REUSEADDR, 1)
+    server_address = '127.0.0.1', 8000
+    server_socket.bind(server_address)
+    server_socket.setblocking(False)
+    server_socket.listen()
+    await listen_for_connection(server_socket, ao.get_event_loop())
+
+
+ao.run(main())
